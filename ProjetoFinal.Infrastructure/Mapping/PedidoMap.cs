7cd1fe6fcec52c4954d02b80ae3da84fc47b614e
Rodrigo Ximenes <rodrigoximenes@tecgraf.puc-rio.br>
2017-08-08 18:42:21 -0300

    Inserção da tabela Pedido

--- ProjetoFinal.Infrastructure/Mapping/PedidoMap.cs
+++ ProjetoFinal.Infrastructure/Mapping/PedidoMap.cs
@@ -7,16 +7,16 @@ namespace ProjetoFinal.Infrastructure.Mapping
     {
         public PedidoMap()
         {
-            //Property(x => x.Id).HasColumnName("Id");
-            //Property(x => x.Atendente).HasColumnName("Atendente");
-            //Property(x => x.Cliente).HasColumnName("Cliente");
-            //Property(x => x.DataEntrega).HasColumnName("DataEntrega");
-            //Property(x => x.DataSolicitacao).HasColumnName("DataSolicitacao");
-            //Property(x => x.EnderecoEntrega).HasColumnName("EnderecoEntrega");
-            //Property(x => x.Itens).HasColumnName("Itens");
-            //Property(x => x.TipoEntrega).HasColumnName("TipoEntrega");
+            Property(x => x.Id).HasColumnName("Id");
+            Property(x => x.Atendente).HasColumnName("Atendente");
+            Property(x => x.Cliente).HasColumnName("Cliente");
+            Property(x => x.DataEntrega).HasColumnName("DataEntrega");
+            Property(x => x.DataSolicitacao).HasColumnName("DataSolicitacao");
+            Property(x => x.EnderecoEntrega).HasColumnName("EnderecoEntrega");
+            Property(x => x.Itens).HasColumnName("Itens");
+            Property(x => x.TipoEntrega).HasColumnName("TipoEntrega");
 
-            //ToTable("Pedido");
+            ToTable("Pedido");
         }
     }
 }
