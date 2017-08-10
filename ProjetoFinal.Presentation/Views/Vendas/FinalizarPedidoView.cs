14b93d406574099222e3861dc9c14be61a836b92
Rodrigo Ximenes <rodrigoximenes@tecgraf.puc-rio.br>
2017-08-10 15:56:22 -0300

    finalizarpedido

--- ProjetoFinal.Presentation/Views/Vendas/FinalizarPedidoView.cs
+++ ProjetoFinal.Presentation/Views/Vendas/FinalizarPedidoView.cs
@@ -23,9 +23,6 @@ namespace ProjetoFinal.Presentation.Views.Vendas
             _itemRepository = LaloKernel.GetInstance<IItemRepository>();
             _clienteRepository = LaloKernel.GetInstance<IClienteRepository>();
 
-            //var _pedido = new Pedido();
-            //var _cliente = new Cliente();
-
             InitializeComponent();
             BindComboEntregas();
 
@@ -44,76 +41,77 @@ namespace ProjetoFinal.Presentation.Views.Vendas
             txtBoxFormaEntrega.Text = pedidoViewModel.TipoEntrega;
             txtBoxEndereco.Text = pedidoViewModel.EnderecoEntrega;
             txtBoxValorPedido.Text = pedidoViewModel.getValorTotal().ToString();
-    }
-
-    #region
-
-    private void BindComboEntregas()
-    {
-        var entregasList = new List<string>();
-
-        entregasList.Add("À Vista");
-        entregasList.Add("Agendada");
-        cmbBoxFormaPagamento.DataSource = entregasList;
-    }
-
-    private void CalculoValorTotal()
-    {
-        var valorPedido = decimal.Parse(txtBoxValorPedido.Text);
-
-        if (Equals(txtBoxValorDesconto.Text, string.Empty))
-        {
-            txtBoxValorTotal.Text = valorPedido.ToString();
-        }
-        else
-        {
-            txtBoxValorTotal.Text = (valorPedido - (valorPedido * (decimal.Parse(txtBoxValorDesconto.Text) / 100))).ToString();
-        }
-    }
-
-    private void CalculoTroco()
-    {
-        var valorRecebido = decimal.Parse(txtBoxValorRecebido.Text);
-
-        if (valorRecebido < decimal.Parse(txtBoxValorPedido.Text))
-        {
-            MessageBox.Show("Não é possível pagar pedido com o valor recebido");
-            return;
         }
 
-        txtBoxTroco.Text = (valorRecebido - decimal.Parse(txtBoxValorPedido.Text)).ToString();
+        #region
+
+        private void BindComboEntregas()
+        {
+            var entregasList = new List<string>();
+
+            entregasList.Add("À Vista");
+            entregasList.Add("Agendada");
+            cmbBoxFormaPagamento.DataSource = entregasList;
+        }
+
+        private void CalculoValorTotal()
+        {
+            var valorPedido = decimal.Parse(txtBoxValorPedido.Text);
+
+            if (Equals(txtBoxValorDesconto.Text, string.Empty))
+            {
+                txtBoxValorTotal.Text = valorPedido.ToString();
+            }
+            else
+            {
+                txtBoxValorTotal.Text = (valorPedido - (valorPedido * (decimal.Parse(txtBoxValorDesconto.Text) / 100))).ToString();
+            }
+        }
+
+        private void CalculoTroco()
+        {
+            var valorRecebido = decimal.Parse(txtBoxValorRecebido.Text);
+
+            if (valorRecebido < decimal.Parse(txtBoxValorPedido.Text))
+            {
+                MessageBox.Show("Não é possível pagar pedido com o valor recebido");
+                return;
+            }
+
+            txtBoxTroco.Text = (valorRecebido - decimal.Parse(txtBoxValorPedido.Text)).ToString();
+        }
+
+        #endregion
+
+        #region Events
+
+        private void btnCancelar_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtBoxValorRecebido_Leave(object sender, System.EventArgs e)
+        {
+            CalculoTroco();
+            CalculoValorTotal();
+        }
+
+        private void txtBoxValorDesconto_Leave(object sender, System.EventArgs e)
+        {
+            CalculoTroco();
+            CalculoValorTotal();
+        }
+
+        private void btnFinalizarPedido_Click(object sender, System.EventArgs e)
+        {
+            if (Equals(txtBoxValorRecebido.Text, null)) return;
+            _clienteRepository.Add(_cliente);
+            _pedidoRepository.Add(_pedido);
+
+            MessageBox.Show("Pedido Finalizado");
+        }
+
+        #endregion
+
     }
-
-    #endregion
-
-    #region Events
-
-    private void btnCancelar_Click(object sender, System.EventArgs e)
-    {
-        this.Close();
-    }
-
-    private void txtBoxValorRecebido_Leave(object sender, System.EventArgs e)
-    {
-        CalculoTroco();
-        CalculoValorTotal();
-    }
-
-    private void txtBoxValorDesconto_Leave(object sender, System.EventArgs e)
-    {
-        CalculoTroco();
-        CalculoValorTotal();
-    }
-
-    private void btnFinalizarPedido_Click(object sender, System.EventArgs e)
-    {
-        _clienteRepository.Add(_cliente);
-        _pedidoRepository.Add(_pedido);
-
-        MessageBox.Show("Pedido Finalizado");
-    }
-
-    #endregion
-
-}
 }
